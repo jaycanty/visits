0d8bf99dc317ccc4b85134bc030fb86f79491235
jay <jay@jays-MBP.hsd1.nj.comcast.net>
2019-01-23 17:57:15 -0500

    Add log

--- Visits/Model.swift
+++ Visits/Model.swift
@@ -46,4 +46,25 @@ class Model {
            UserDefaults.standard.set(data, forKey: key)
         }
     }
+    
+    private func printVisits() {
+        for visit in visits {
+            var log = ""
+            if let placemark = visit.placemark {
+                log = String(format: "%@ - %@, %@\nARRIVAL - %@\nDEPARTURE - %@",
+                             placemark.thoroughfare ?? "unkown",
+                             placemark.subAdministrativeArea ?? "unknown",
+                             placemark.administrativeArea ?? "unknow",
+                             visit.visit.arrivalDate.description(with: NSLocale.current),
+                             visit.visit.departureDate.description(with: NSLocale.current))
+            } else {
+                log = String(format: "%d, %d\nARRIVAL - %@\nDEPARTURE - %@",
+                             visit.visit.coordinate.latitude,
+                             visit.visit.coordinate.longitude,
+                             visit.visit.arrivalDate.description(with: NSLocale.current),
+                             visit.visit.departureDate.description(with: NSLocale.current))
+            }
+            print(log)
+        }
+    }
 }
